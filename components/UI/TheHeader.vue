dec4bd6fb60f5238cebc31fc589d31e54f5cb1bd
yahia-raheem <eng.yahiaraheem.93@outlook.com>
2021-05-04 15:25:29 +0200

    added the doctor profile verification notice

--- components/UI/TheHeader.vue
+++ components/UI/TheHeader.vue
@@ -2,6 +2,10 @@
   <div
     :class="{ 'nav-container': true, 'home-header': $route.fullPath == '/' }"
   >
+    <div class="notice" v-if="docProfStatus == false">
+      Please verify your profile as soon as possible to fully enjoy our
+      platform's services
+    </div>
     <div class="white-gradient" v-if="$route.fullPath == '/'">
       <get-img :imgid="navBgGradient" />
     </div>
@@ -113,7 +117,14 @@ export default {
   computed: {
     ...mapGetters({
       socialMedia: "general/socialMedia"
-    })
+    }),
+    docProfStatus() {
+      if (this.isLoggedIn && this.$auth.user.doctor_profile != null) {
+        return this.$auth.user.doctor_profile.status == 1 ? true : false;
+      } else {
+        return null;
+      }
+    }
   },
   methods: {
     getOption(optionId) {
@@ -191,6 +202,18 @@ export default {
     .navbar-toggler-icon {
       background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30'%3E%3Cpath stroke='rgba(0, 0, 0, 0.5)' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
     }
+  }
+  .notice {
+    background-color: red;
+    position: relative;
+    z-index: 2;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    padding: 8px;
+    font-weight: 600;
   }
   .social {
     padding: 0;
